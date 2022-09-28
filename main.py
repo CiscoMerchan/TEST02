40e87597b510c45257f5580c1c05033c6a6156f6
Franciscoelias <franciscoeliasm@yahoo.com>
2022-09-28 12:18:50 +1000

    Initial presentation

--- main.py
+++ main.py
@@ -1,22 +1,12 @@
 from flask import Flask, render_template
-from flask_wtf import FlaskForm
-from wtforms import SubmitField,StringField
 
-class LoginForm(FlaskForm):
-    email = StringField(label='email')
-    password = StringField(label='pasword')
-    submit = SubmitField(label='Log')
 app = Flask(__name__)
 app.config['SECRET_KEY']='seCRET_KEY_VERY_SECURE'
 @app.route("/")
 def home():
     return render_template('home.html')
 
-@app.route("/login")
-def login():
-    form = LoginForm()
 
-    return render_template('login.html',form=form)
 
 
 if __name__ == '__main__':
